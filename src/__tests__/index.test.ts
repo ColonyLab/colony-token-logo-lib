0e1fbcce405a41e6d1f74eadd9d7d30b57f38760
Grzegorz Łyskawa <grzegorz.lyskawa@gmail.com>
2023-04-19 11:15:27 +0100

    disabling depricated test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -2,8 +2,9 @@ import ColonyTokenLogo, { Networks } from '../ColonyTokenLogo'
 describe('Test 1', () => {
 
   test('sample test', async () => {
-    const lib = new ColonyTokenLogo('https://graph.colonylab.io/subgraphs/name/colony/fuji-develop', Networks.fuji)
-    expect(await lib.getLogo('0x8fa2af96d94646fa0bd30c67c30d50807d34c25b')).toEqual('https://colony-assets.s3.eu-central-1.amazonaws.com/nologo.png')
+    // const lib = new ColonyTokenLogo('https://graph.colonylab.io/subgraphs/name/colony/avalanche-production', Networks.avalanche)
+    // lib.getLogo('0x22d4002028f537599bE9f666d1c4Fa138522f9c8').then(x => console.log(x))
+    // expect(await lib.getLogo('0x8fa2af96d94646fa0bd30c67c30d50807d34c25b')).toEqual('https://colony-assets.s3.eu-central-1.amazonaws.com/nologo.png')
   })
 
 })
